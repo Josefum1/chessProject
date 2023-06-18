e3752c13f60907ae5dec06ab3092f3b78ef7c79e
Josefum1 <juninho.jads@gmail.com>
2023-06-18 15:43:00 -0300

    Começando a implementação dos movimentos possíveis de uma peça

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -8,7 +8,15 @@ public class King extends ChessPiece {
     public King(Board board, Color color) {
         super(board, color);
     }
-    public String toString(){
+
+    @Override
+    public String toString() {
         return "K";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
