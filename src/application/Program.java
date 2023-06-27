164d7158174fbd92255483fd67c6b8ee130daf42
Josefum1 <juninho.jads@gmail.com>
2023-06-27 19:53:14 -0300

    Implantando a lógica do check e arrumando um bug no ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,17 +39,14 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-            }
-            catch (ChessException e){
+            } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            }
-            catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
         }
-
 
     }
 }
